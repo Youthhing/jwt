49c48cd551bf2039e887f6f61ac95a904b10a9e0
Youth <boysoeng@naver.com>
2023-11-27 00:51:51 +0900

    fix: filter 수정

--- src/main/java/com/youth/jwt/config/FilterConfig.java
+++ src/main/java/com/youth/jwt/config/FilterConfig.java
@@ -1,7 +1,7 @@
 package com.youth.jwt.config;
 
-import com.youth.jwt.filter.MyFilter1;
 import com.youth.jwt.filter.MyFilter2;
+import com.youth.jwt.filter.MyFilter3;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,18 +10,20 @@ import org.springframework.context.annotation.Configuration;
 public class FilterConfig {
 
     @Bean
-    public FilterRegistrationBean<MyFilter1> filter1() {
-        FilterRegistrationBean<MyFilter1> bean = new FilterRegistrationBean<>(new MyFilter1());
+    public FilterRegistrationBean<MyFilter2> filter2() {
+        FilterRegistrationBean<MyFilter2> bean = new FilterRegistrationBean<>(new MyFilter2());
         bean.addUrlPatterns("/*");
         bean.setOrder(0);
         return bean;
     }
 
     @Bean
-    public FilterRegistrationBean<MyFilter2> filter2() {
-        FilterRegistrationBean<MyFilter2> bean = new FilterRegistrationBean<>(new MyFilter2());
+    public FilterRegistrationBean<MyFilter3> filter3() {
+        FilterRegistrationBean<MyFilter3> bean = new FilterRegistrationBean<>(new MyFilter3());
         bean.addUrlPatterns("/*");
         bean.setOrder(1);
         return bean;
     }
+
+
 }
